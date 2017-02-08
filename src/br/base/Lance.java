709a72a917596d6e3ec8cb521ad0ba8a2a81f3cd
Victor Hugo Lopes <hullopes@hotmail.com>
2017-02-08 20:53:43 -0200

    Commit Inicial do Projeto

---
+++ src/br/base/Lance.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.base;
+
+/**
+ *
+ * @author tatuapu
+ */
+public class Lance {
+
+	private Usuario usuario;
+	private double valor;
+	
+	public Lance(Usuario usuario, double valor) {
+		this.usuario = usuario;
+		this.valor = valor;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+	
+	
+	
+}
