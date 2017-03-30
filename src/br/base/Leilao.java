4c943dd6c3a204aea92a8ce6ccb93a9a8f4e6004
Victor Hugo Lopes <hullopes@hotmail.com>
2017-03-30 10:41:06 -0300

    Criação da classe Leilão

--- src/br/base/Leilao.java
+++ src/br/base/Leilao.java
@@ -5,10 +5,36 @@
  */
 package br.base;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author tatuapu
  */
 public class Leilao {
-    
+        private String descricao;
+	private List<Lance> lances;
+	
+	public Leilao(String descricao) {
+		this.descricao = descricao;
+		this.lances = new ArrayList<Lance>();
+	}
+        public void propoe(Lance lance) {
+		getLances().add(lance);
+	}
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @return the lances
+     */
+    public List<Lance> getLances() {
+        return lances;
+    }
 }
